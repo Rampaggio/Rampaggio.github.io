33ef45327b3449144a38e393fb69e133d650fddf
Rampaggio <rampyatx@gmail.com>
2015-11-11 20:50:44 -0600

    got the script linked

--- js/my-animation-scroll.js
+++ js/my-animation-scroll.js
@@ -8,6 +8,23 @@ $(document).ready(function () {
 
             $('.titlesrt').addClass('fadeInRight');
         }
+        
+        if(verticalScroll >= 500) {
+
+            $('.titlesrt2').addClass('fadeInRight');
+        }
+        
+         if(verticalScroll >= 800) {
+
+            $('.titlesrt3').addClass('fadeInRight');
+        }
+        
+        if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+    {
+        $('.opaque-navbar').addClass('opaque');
+    } else {
+        $('.opaque-navbar').removeClass('opaque');
+    }
 
     });
 });
